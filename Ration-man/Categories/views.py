9c8b9eb4bbfdc4731d86c2668171f30a964bba4b
Manigandan Kasimani <manigandankasimani@gmail.com>
2020-11-24 16:24:43 +0530

    Restarted project

--- Ration-man/Categories/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-@login_required
-def category(request):
-    return render(request,'Categories/category.html',{'title': 'Categories'})
